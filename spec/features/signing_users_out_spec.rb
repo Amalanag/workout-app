613855bda1d95156da32a91e7150ec3baf20575f
Amala <amalarails@gmail.com>
2015-11-19 21:26:06 +0000

    Impelement Sign out feature

---
+++ spec/features/signing_users_out_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "Signing out signed-in-users" do
+	before do
+			@amala = User.create!(email: "amalarails@example.com",password: "123456789")
+		visit "/"
+		click_link "Sign in"
+		fill_in "Email",with:@amala.email
+		fill_in "Password",with:@amala.password
+		click_button "Log in"
+	end
+	scenario do
+		visit "/"
+		click_link "Sign out"
+		expect(page).to have_content("Signed out successfully")
+	end
+end

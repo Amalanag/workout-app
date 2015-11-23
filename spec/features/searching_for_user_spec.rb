f74db7ca432639d14e0621720de08a5097cce4a4
Amala <amalarails@gmail.com>
2015-11-23 22:07:47 +0000

    Implement searching

---
+++ spec/features/searching_for_user_spec.rb
@@ -0,0 +1,20 @@
+require 'rails_helper'
+
+RSpec.feature "Searching for User" do
+
+	before do
+		@amala = User.create(first_name: "Amala",last_name: "Nivas", email: "amalarails@gmail.com",password:"123456789")
+		@amu = User.create(first_name: "Naga",last_name: "Amala", email: "amala.b@gmail.com",password:"123456789")
+	end
+
+	scenario "with existing name returns all those users" do
+		visit "/"
+
+		fill_in "search_name",with: "Amala"
+		click_button "Search"
+
+		expect(page).to have_content(@amala.full_name)
+		expect(page).to have_content(@amu.full_name)
+		expect(current_path).to eq("/dashboard/search")
+	end
+end
